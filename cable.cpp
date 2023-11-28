2c78036269907c2b675168a730fc0fb64bfe1817
Thomas <thomaskuhrke24@gmail.com>
2023-11-28 15:26:07 -0700

    Provided theoretical implementation for the methods, and wrote comments to track algorithm.

--- cable.cpp
+++ cable.cpp
@@ -2,38 +2,88 @@
 
 Cable::Cable(QPoint startPos, QColor cableColor)
 {
-    cableEndPos = startPos;
+    //Set the color of the cables
     this->cableColor = cableColor;
     cableEndPosColor = cableColor.darker(20);
-    path.append(cableEndPos);
+
+    //Append the starting position to the path list
+    path.append(startPos);
+
+    cableEndPos = &(path.at(0));
+    scaleFactor = 8; // Default scaleFactor
     canDraw = false;
-//    QPoint cableEndPos;
-//    QColor cableColor;
-//    QColor cableEndPosColor;
-//    QList<QPoint> path;
 }
 
 void Cable::mousePressed(QImage &image, const QPoint &mouseLocation)
 {
-    if ((mouseLocation.x() == cableEndPos.x()) && mouseLocation.y() == cableEndPos.y()){
+    if ((mouseLocation.x() == cableEndPos->x()) && mouseLocation.y() == cableEndPos->y()){
+        // Start the Painter
+        painter.begin(&image);
+
+        // Set the pen to paint based on the cable color
+        QPen cursorPen = painter.pen();
+        cursorPen.setColor(cableColor);
+        painter.setPen(cursorPen);
+
+        //Set ability to draw to true
         canDraw = true;
     }
 }
 
 void Cable::mouseReleased(QImage &image, const QPoint &mouseLocation)
 {
+    //Set the end of the cable to the last item in the path list, and set its pixel color in the canvas to a darker version.
+    cableEndPos = &(path.at(path.size() - 1));
+    image.setPixelColor(mapToImageCoordinates(mouseLocation, scaleFactor), cableEndPosColor);
+
+    //Stop the painter and remove the ability to draw
     canDraw = false;
+    painter.end();
 }
 
-void Cable::mouseMoved(QImage &image, const QPoint &mouseLocation)
+void Cable::mouseMoved(const QPoint &mouseLocation)
 {
+    //Convert mouse coordinates into pixel coordinates.
+    QPoint startPoint = mapToImageCoordinates(mouseLocation, scaleFactor);
+    QPoint endPoint = mapToImageCoordinates(mouseLocation, scaleFactor);
+
+    //If the user starts at a drawable pixel, let it draw
     if (canDraw){
-        //Draw Horizontally or vertically
-        //Add location(s) to path
+        //Calculate change in y and x, and determine whether the user is drawing a straight line.
+        int dx = startPoint.x() - endPoint.x();
+        int dy = startPoint.y() - endPoint.y();
+
+        //If the user is drawing in a straight line, draw the line and record the coordinates.
+        if (dx == 0 || dy == 0){
+            drawLine(startPoint, endPoint);
+            appendToPath(startPoint, endPoint, dy, dx);
+        }
     }
 }
 
-void Cable::drawLine()
+void Cable::drawLine(const QPoint& startPoint, const QPoint& endPoint)
 {
+    painter.drawLine(startPoint, endPoint);
+}
 
+void Cable::appendToPath(const QPoint &startPoint, const QPoint &endPoint, int &dy, int &dx){
+    //Determine whether the line is horizontal or vertical, and add all the pixel coordinates between the 2 points selected.
+    if (dx == 0){
+        for (int y = startPoint.y(); y <= endPoint.y(); y++){
+            path.append(QPoint(startPoint.x(), y));
+        }
+    }
+    else if (dy == 0){
+        for (int x = startPoint.x(); x <= endPoint.x(); x++){
+            path.append(QPoint(x, startPoint.y()));
+        }
+    }
+}
+
+void Cable::changeScaleFactor(int newScaleFactor){
+    scaleFactor = newScaleFactor;
+}
+
+QPoint Cable::mapToImageCoordinates(const QPoint &point, int scaleFactor){
+    return QPoint(point.x() / scaleFactor, point.y() / scaleFactor);
 }
