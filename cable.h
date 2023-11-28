2e4e3d4b3bd3eac7260836322c721ffe11b97561
Thomas <thomaskuhrke24@gmail.com>
2023-11-28 15:40:26 -0700

    Wrote header comments.

--- cable.h
+++ cable.h
@@ -11,33 +11,34 @@ class Cable
 {
 public:
     /**
-     * @brief Cable
-     * @param startPos
-     * @param cableColor
+     * @brief Cable constructor for the cable class.
+     * @param startPos position of the initial cable.
+     * @param cableColor color of the cable.
      */
     Cable(QPoint startPos, QColor cableColor);
 
     /**
-     * @brief mousePressed
-     * @param mousePos
+     * @brief mousePressed checks if the user is at the end of a cable, and it is, lets the user extend(or draw) the cable.
+     * @param mousePos position of the mouse.
      */
     void mousePressed(QImage&, const QPoint& mousePos);
 
     /**
-     * @brief mouseReleased
-     * @param mousePos
+     * @brief mouseReleased stops extending the cable when the user releases the mouse button that was pressed.
+     * @param mousePos position of the mouse.
      */
     void mouseReleased(QImage&, const QPoint& mousePos);
 
     /**
-     * @brief mouseMoved
-     * @param mousePos
+     * @brief mouseMoved lets the user draw in the image if the mouse is pressed and moving, and
+     * if they started drawing from the end of a cable.
+     * @param mousePos position of the mouse.
      */
     void mouseMoved(const QPoint &mousePos);
 
     /**
-     * @brief changeScaleFactor
-     * @param newScaleFactor
+     * @brief changeScaleFactor changes the scale factor to the desired scaleFactor.
+     * @param newScaleFactor the new scale factor.
      */
     void changeScaleFactor(int newScaleFactor);
 
@@ -53,26 +54,26 @@ protected:
 
 private:
     /**
-     * @brief drawLine
-     * @param startPoint
-     * @param endPoint
+     * @brief drawLine draws a horizontal or vertical line between 2 points.
+     * @param startPoint the starting point of a line.
+     * @param endPoint the ending point of a line.
      */
     void drawLine(const QPoint& startPoint, const QPoint& endPoint);
 
     /**
-     * @brief appendToPath
-     * @param startPoint
-     * @param endPoint
-     * @param dy
-     * @param dx
+     * @brief appendToPath gets all the points generated between 2 points in an image, and appends them to the path list.
+     * @param startPoint the starting point of a line.
+     * @param endPoint the ending point of a line.
+     * @param dy change in y between the 2 points.
+     * @param dx change in x between the 2 points.
      */
     void appendToPath(const QPoint& startPoint, const QPoint& endPoint, int& dy, int& dx);
 
     /**
-     * @brief mapToImageCoordinates
-     * @param point
-     * @param scaleFactor
-     * @return
+     * @brief mapToImageCoordinates converts mouse coordinates to the model's image coordinates.
+     * @param point the point to be converted.
+     * @param scaleFactor factor to scale the point to the model's image coordinates.
+     * @return a point in the model's image.
      */
     QPoint mapToImageCoordinates(const QPoint& point, int scaleFactor);
 };
