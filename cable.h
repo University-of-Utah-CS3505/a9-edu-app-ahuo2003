18a02067cb0055af461c3ebe27dc9384c193ce28
Thomas <thomaskuhrke24@gmail.com>
2023-11-28 15:24:59 -0700

    Added the format for header comments to the document, and added necessary methods. Also removed unused parameters.

--- cable.h
+++ cable.h
@@ -5,23 +5,76 @@
 #include <QList>
 #include <QPoint>
 #include <QColor>
+#include <QPainter>
 
 class Cable
 {
 public:
+    /**
+     * @brief Cable
+     * @param startPos
+     * @param cableColor
+     */
     Cable(QPoint startPos, QColor cableColor);
-    void mousePressed(QImage&, const QPoint &mousePos);
-    void mouseReleased(QImage&, const QPoint &mousePos);
-    void mouseMoved(QImage&, const QPoint &mousePos);
 
-private:
-    QPoint cableEndPos;
+    /**
+     * @brief mousePressed
+     * @param mousePos
+     */
+    void mousePressed(QImage&, const QPoint& mousePos);
+
+    /**
+     * @brief mouseReleased
+     * @param mousePos
+     */
+    void mouseReleased(QImage&, const QPoint& mousePos);
+
+    /**
+     * @brief mouseMoved
+     * @param mousePos
+     */
+    void mouseMoved(const QPoint &mousePos);
+
+    /**
+     * @brief changeScaleFactor
+     * @param newScaleFactor
+     */
+    void changeScaleFactor(int newScaleFactor);
+
+
+protected:
+    const QPoint* cableEndPos;
     QColor cableColor;
     QColor cableEndPosColor;
     QList<QPoint> path;
+    QPainter painter;
     bool canDraw;
-    void drawLine();
-    QPoint mapToImageCoordinates(const QPoint &point, int scaleFactor);
+    int scaleFactor;
+
+private:
+    /**
+     * @brief drawLine
+     * @param startPoint
+     * @param endPoint
+     */
+    void drawLine(const QPoint& startPoint, const QPoint& endPoint);
+
+    /**
+     * @brief appendToPath
+     * @param startPoint
+     * @param endPoint
+     * @param dy
+     * @param dx
+     */
+    void appendToPath(const QPoint& startPoint, const QPoint& endPoint, int& dy, int& dx);
+
+    /**
+     * @brief mapToImageCoordinates
+     * @param point
+     * @param scaleFactor
+     * @return
+     */
+    QPoint mapToImageCoordinates(const QPoint& point, int scaleFactor);
 };
 
 #endif // CABLE_H
