5e00d254185551b99309ca2b9e2d2534e3e902bb
haiminhpham <phamhaiminh2003@gmail.com>
2023-12-07 21:27:13 -0700

    Add method comments and getter for input wire(s)

--- NotGate.cpp
+++ NotGate.cpp
@@ -1,47 +1,53 @@
 #include "NotGate.h"
 
-
-NotGate::NotGate(int x, int y) : Gate(x,y){ this->type = GateType::NOT; this->output = Cable(QPoint(x+2, y), cableColor);}
+NotGate::NotGate(int x, int y) : Gate(x, y) {
+  this->type = GateType::NOT;
+  this->output = Cable(QPoint(x + 2, y), cableColor);
+}
 
 NotGate::~NotGate() = default;
 
-GateType NotGate::getType() const{
-    return this->type;
-}
+GateType NotGate::getType() const { return this->type; }
 
 void NotGate::draw(QImage &image) {
-    this->painter.begin(&image);
+  this->painter.begin(&image);
 
-    QPen cursorPen = painter.pen();
-    cursorPen.setColor(Qt::red);
-    painter.setPen(cursorPen);
+  QPen cursorPen = painter.pen();
+  cursorPen.setColor(Qt::red);
+  painter.setPen(cursorPen);
 
-    QPoint center(pos_x, pos_y);
-    painter.drawPoint(center);
-    notGatePixels.append(center);
+  // Get and draw the center of the gate.
+  QPoint center(pos_x, pos_y);
+  painter.drawPoint(center);
+  notGatePixels.append(center);
 
-    QPoint top(center.x(), center.y() + 1);
-    painter.drawPoint(top);
-    notGatePixels.append(top);
+  // Draw gate based on agreed shape.
+  QPoint top(pos_x, pos_y + 1);
+  painter.drawPoint(top);
+  notGatePixels.append(top);
 
-    QPoint bottom(center.x(), center.y() - 1);
-    painter.drawPoint(bottom);
-    notGatePixels.append(bottom);
+  QPoint bottom(pos_x, pos_y - 1);
+  painter.drawPoint(bottom);
+  notGatePixels.append(bottom);
 
-    QPoint right(center.x() + 1, center.y());
-    painter.drawPoint(right);
-    notGatePixels.append(right);
+  QPoint right(pos_x + 1, pos_y);
+  painter.drawPoint(right);
+  notGatePixels.append(right);
 
-    cursorPen.setColor(Qt::yellow);
-    painter.setPen(cursorPen);
+  cursorPen.setColor(Qt::yellow);
+  painter.setPen(cursorPen);
 
-    QPoint inputWire(center.x() - 1, center.y());
-    painter.drawPoint(inputWire);
-    notGatePixels.append(inputWire);
+  // Draw the input wire.
+  QPoint inputWire(pos_x - 1, pos_y);
+  painter.drawPoint(inputWire);
+  notGatePixels.append(inputWire);
 
-    QPoint outputWire(center.x() + 2, center.y());
-    painter.drawPoint(outputWire);
-    notGatePixels.append(outputWire);
+  // Draw the output wire.
+  QPoint outputWire(pos_x + 2, pos_y);
+  painter.drawPoint(outputWire);
+  notGatePixels.append(outputWire);
 
-    this->painter.end();
+  this->painter.end();
 }
+
+QPoint NotGate::getInput() { return QPoint(pos_x - 2, pos_y - 1); }
--- NotGate.h
+++ NotGate.h
@@ -3,16 +3,33 @@
 
 #include "Gate.h"
 
+/**
+ * @brief The NotGate class provide a visible NOT gate using QPoint
+ * to draw it onto the canvas. the class also provide logic between
+ * NOT gate and cable.
+ */
 class NotGate : public Gate {
-
 public:
-    NotGate(int x, int y);
-    ~NotGate() override;
-    QList<QPoint> notGatePixels;
+  NotGate(int x, int y);
+  ~NotGate() override;
+  QList<QPoint> notGatePixels;
+  /**
+   * @brief draw the method draw the NOT gate onto the canvas.
+   * @param image the canvas to draw onto.
+   */
+  void draw(QImage &image) override;
 
-    void draw(QImage &image) override;
+  /**
+   * @brief getInput get the input wire of the NOT gate.
+   * @return the position(x,y) of the input wire.
+   */
+  QPoint getInput();
 
-    GateType getType() const override;
+  /**
+   * @brief getType get the type of the gate.
+   * @return GateType::NOT.
+   */
+  GateType getType() const override;
 };
 
 #endif // NOTGATE_H
