c82bd2a96dcf5dfda25a405ce3975885c1165abc
haiminhpham <phamhaiminh2003@gmail.com>
2023-12-07 21:27:16 -0700

    Add method comments and getter for input wire(s)

--- OrGate.h
+++ OrGate.h
@@ -3,16 +3,39 @@
 
 #include "Gate.h"
 
+/**
+ * @brief The OrGate class provide a visible OR gate using QPoint
+ * to draw it onto the canvas. the class also provide logic between
+ * OR gate and cable.
+ */
 class OrGate : public Gate {
-
 public:
-    OrGate(int x, int y);
-    ~OrGate() override;
-    QList<QPoint> orGatePixels;
+  OrGate(int x, int y);
+  ~OrGate() override;
+  QList<QPoint> orGatePixels;
+  /**
+   * @brief draw the method draw the OR gate onto the canvas.
+   * @param image the canvas to draw onto.
+   */
+  void draw(QImage &image) override;
 
-    void draw(QImage &image) override;
+  /**
+   * @brief getInput1 get the first input wire of the OR gate.
+   * @return the position(x,y) of the first input wire.
+   */
+  QPoint getInput1();
 
-    GateType getType() const override;
+  /**
+   * @brief getInput2 get the second input wire of the OR gate.
+   * @return the position(x,y) of the second input wire.
+   */
+  QPoint getInput2();
+
+  /**
+   * @brief getType get the type of the gate.
+   * @return GateType::OR.
+   */
+  GateType getType() const override;
 };
 
 #endif // ORGATE_H
