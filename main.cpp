310f0bd85db29b34094d43170b22ec33876c05fa
haiminhpham <phamhaiminh2003@gmail.com>
2023-12-07 21:26:50 -0700

    Formatting Purpose

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
-#include "MainWindow.h"
 #include "Canvas.h"
+#include "MainWindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     Canvas canvas;
     Model model;
