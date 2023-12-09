5d2c010680d11b17ec1a148e5dbb52c25df911b8
Thomas <thomaskuhrke24@gmail.com>
2023-12-08 17:01:25 -0700

    Implemented new constructor of InGate into the first 3 levels(tutorial), and commented the rest for the Level group to implement.

--- Level.cpp
+++ Level.cpp
@@ -34,38 +34,37 @@ void Level::loadLevelData(int levelNumber)
     switch (levelNumber) {
     case 1:
         gates.append(new AndGate(15, 10));
-        gates.append(new InGate(5,5, Qt::red));
-        gates.append(new InGate(5,15, Qt::red));
+        gates.append(new InGate(5,5, false, Qt::red));
+        gates.append(new InGate(5,15, true, Qt::green));
         gates.append(new OutGate(30,10));
         break;
 
     case 2:
         gates.append(new OrGate(15, 10));
-        gates.append(new InGate(5,5, Qt::red));
-        gates.append(new InGate(5,15, Qt::red));
+        gates.append(new InGate(5,5, false, Qt::red));
+        gates.append(new InGate(5,15, true, Qt::green));
         gates.append(new OutGate(30,10));
         break;
 
     case 3:
         gates.append(new NotGate(15, 10));
-        gates.append(new InGate(5,5, Qt::red));
-        gates.append(new InGate(5,15, Qt::red));
+        gates.append(new InGate(5,10, false,  Qt::red));
         gates.append(new OutGate(30,10));
         break;
 
     case 4:
         gates.append(new NotGate(7, 1));
-        gates.append(new InGate(1,1, Qt::red));
-        gates.append(new InGate(1,5, Qt::red));
+//        gates.append(new InGate(1,1, Qt::red));
+//        gates.append(new InGate(1,5, Qt::red));
         gates.append(new OutGate(25, 3));
         gates.append(new AndGate(15, 3));
         break;
 
     case 5:
         gates.append(new NotGate(6, 1));
-        gates.append(new InGate(1,1, Qt::red));
-        gates.append(new InGate(1,5, Qt::red));
-        gates.append(new InGate(1,9, Qt::red));
+//        gates.append(new InGate(1,1, Qt::red));
+//        gates.append(new InGate(1,5, Qt::red));
+//        gates.append(new InGate(1,9, Qt::red));
         gates.append(new OutGate(30, 5));
         gates.append(new AndGate(12, 3));
         gates.append(new AndGate(22, 5));
@@ -74,10 +73,10 @@ void Level::loadLevelData(int levelNumber)
         gates.append(new NotGate(6, 1));
         gates.append(new NotGate(6, 9));
         gates.append(new NotGate(24, 7));
-        gates.append(new InGate(1,1, Qt::red));
-        gates.append(new InGate(1,5, Qt::red));
-        gates.append(new InGate(1,9, Qt::red));
-        gates.append(new InGate(1,13, Qt::red));
+//        gates.append(new InGate(1,1, Qt::red));
+//        gates.append(new InGate(1,5, Qt::red));
+//        gates.append(new InGate(1,9, Qt::red));
+//        gates.append(new InGate(1,13, Qt::red));
         gates.append(new OutGate(31, 7));
         gates.append(new AndGate(12, 3));
         gates.append(new AndGate(18, 7));
@@ -88,10 +87,10 @@ void Level::loadLevelData(int levelNumber)
         gates.append(new NotGate(6, 1));
         gates.append(new NotGate(6, 9));
         gates.append(new NotGate(24, 7));
-        gates.append(new InGate(1,5, Qt::red));
-        gates.append(new InGate(1,10, Qt::red));
-        gates.append( new InGate(1,15, Qt::red));
-        gates.append(new InGate(1,20, Qt::red));
+//        gates.append(new InGate(1,5, Qt::red));
+//        gates.append(new InGate(1,10, Qt::red));
+//        gates.append( new InGate(1,15, Qt::red));
+//        gates.append(new InGate(1,20, Qt::red));
         gates.append(new OutGate(31, 7));
         gates.append(new AndGate(12, 3));
         gates.append(new AndGate(18, 7));
@@ -103,10 +102,10 @@ void Level::loadLevelData(int levelNumber)
         gates.append(new NotGate(6, 1));
         gates.append(new NotGate(12, 25));
         gates.append(new NotGate(20, 28));
-        gates.append(new InGate(1,1, Qt::red));
-        gates.append(new InGate(1,30, Qt::red));
-        gates.append(new InGate(1,13, Qt::red));
-        gates.append(new InGate(1,23, Qt::red));
+//        gates.append(new InGate(1,1, Qt::red));
+//        gates.append(new InGate(1,30, Qt::red));
+//        gates.append(new InGate(1,13, Qt::red));
+//        gates.append(new InGate(1,23, Qt::red));
         gates.append(new OutGate(31, 15));
         gates.append(new AndGate(12, 3));
         gates.append(new OrGate(18, 13));
@@ -119,11 +118,11 @@ void Level::loadLevelData(int levelNumber)
         gates.append(new NotGate(24, 7));
         gates.append(new NotGate(12, 25));
         gates.append(new NotGate(20, 28));
-        gates.append(new InGate(1,1, Qt::red));
-        gates.append(new InGate(1,5, Qt::red));
-        gates.append(new InGate(1,30, Qt::red));
-        gates.append(new InGate(1,13, Qt::red));
-        gates.append(new InGate(1,23, Qt::red));
+//        gates.append(new InGate(1,1, Qt::red));
+//        gates.append(new InGate(1,5, Qt::red));
+//        gates.append(new InGate(1,30, Qt::red));
+//        gates.append(new InGate(1,13, Qt::red));
+//        gates.append(new InGate(1,23, Qt::red));
         gates.append(new OutGate(31, 15));
         gates.append(new AndGate(12, 3));
         gates.append(new AndGate(18, 13));
