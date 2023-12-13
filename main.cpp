e1347321aa915403c2eb1f69e12f49390e79ef0b
Thomas <thomaskuhrke24@gmail.com>
2023-12-12 21:34:23 -0700

    Added application's description, as well as the name of the assignment, and the people involved in it.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include "MainWindow.h"
 #include <QApplication>
-
+/*
+ * Names: Hunter Keating, Hai Minh Pham, Thomas Kuhrke Limia, Andy Huo, Nam Nguyen, Emmanuel Luna
+ * Assignment: A8: Sprite Editor
+ * Description: This program is an educational application that teaches how basic
+ * logical gates work. The application contains 3 tutorial levels that shows the
+ * user how each gate works(AND/OR/NOT), followed by 6 levels that get more difficult
+ * as the player progresses in the game.
+ */
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     Model model;
